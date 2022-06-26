4cc23278fb109241efc6a802997fe2f18733e4a0
Ayah Ajlouni <100513082+AyahAjlouni@users.noreply.github.com>
2022-06-26 09:59:18 +0300

    Add files via upload

---
+++ Classes/Games.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TermProjectCS.Classes
+{
+    public class Games
+    {
+        public int Level;
+        public int Score;
+        public int NumOfSuccessfulClicks;
+        public int NumOfWrongClicks;
+        public int Duration;
+        public DateTime date;
+        public string[] steps ;
+        public Games()
+        {
+            Level = 1;
+            Score = 0;
+            NumOfSuccessfulClicks = 0;
+            NumOfWrongClicks = 0;
+            Duration = 0;
+            date = DateTime.Today;
+            steps = new string[4];
+        }
+        public Games(Games g)
+        {
+            Level = g.Level;
+            Score = g.Score;
+            NumOfSuccessfulClicks = g.NumOfSuccessfulClicks;
+            NumOfWrongClicks = g.NumOfWrongClicks;
+            Duration = g.Duration;
+            date = g.date;
+            steps = new string[4];
+            for (int i=0;i<4;i++)
+            {
+                steps[i] = g.steps[i];
+            }
+        }
+    }
+}
---
+++ Classes/Players.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace TermProjectCS.Classes
+{
+    public class Players
+    {
+        public string Name;
+        public int Age;
+        public string Gender;
+        public List<Games> G;
+        public Color colorofbg;
+        public int MaxDuration , MinDuration ,MaxScore , MinScore,TotalDuration;
+        public Players()
+        {
+            colorofbg = Color.Orange;
+            G = new List<Games>();
+            Name = "UnKnown";
+            Gender = "Male";
+            Age = 10;
+            MaxDuration = 0;
+            MinDuration = 100;
+            MaxScore = 0;
+            MinScore = 100;
+            TotalDuration = 0;
+        }
+        public Players(Players p)
+        {
+            colorofbg = p.colorofbg;
+            Name = p.Name;
+            Gender = p.Gender;
+            Age = p.Age;
+            MaxDuration = p.MaxDuration;
+            MinDuration = p.MinDuration;
+            MaxScore = p.MaxScore;
+            MinScore = p.MinScore;
+            TotalDuration = p.TotalDuration;
+            G = new List<Games>();
+            for(int i=0;i<p.G.Count;i++)
+            {
+                G.Add(new Games(p.G[i]));
+            }
+        }
+        public void setData()
+        {
+            TotalDuration = 0;
+            foreach (Games g in G)
+            {
+                if (g.Duration >= MaxDuration)
+                    MaxDuration = g.Duration;
+            }
+            foreach (Games g in G)
+            {
+                if (g.Duration <= MinDuration)
+                    MinDuration = g.Duration;
+            }
+            foreach (Games g in G)
+            {
+                if (g.Score >= MaxScore)
+                    MaxScore = g.Score;
+            }
+            foreach (Games g in G)
+            {
+                if (g.Score <= MinScore)
+                    MinScore = g.Score;
+            }
+            foreach (Games g in G)
+            { 
+                TotalDuration = TotalDuration + g.Duration;
+            }
+        }
+    }
+}
